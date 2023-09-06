fc1d76cba78e94821fbff2e38c7c87c7ab121bb7
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-09-06 15:55:59 +0300

    update

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,7 +1,52 @@
 const refs = {
-  startBtn: document.querySelector('button[data-action-start]'),
-  stopBtn: document.querySelector('button[data-action-stop]'),
-  clockface: document.querySelector('.js-clockface'),
+  clockFaceEl: document.querySelector('.js-clockface'),
+  startBtn: document.querySelector('[data-action-start]'),
+  stopBtn: document.querySelector('[data-action-stop]'),
 };
 
-class Stopwatch {}
+const stopwatch = {
+  initTime: 0,
+  intervalId: 123,
+  isActive: false,
+  start() {
+    if (this.isActive === true) {
+      return;
+    }
+    this.initTime = Date.now(); // 102400603492423
+    this.intervalId = setInterval(() => {
+      this.tick();
+    }, 1000);
+    this.isActive = true;
+  },
+  stop() {
+    if (this.isActive === false) {
+      return;
+    }
+    this.isActive = false;
+    clearInterval(this.intervalId);
+  },
+  render(data) {
+    const { seconds, minutes, hours } = this.parseData(data);
+    refs.clockFaceEl.textContent = `${hours}:${minutes}:${seconds}`;
+  },
+  tick() {
+    const diff = Date.now() - this.initTime;
+    this.render(diff);
+  },
+  parseData(time) {
+    // 30000; 30
+    // 30000; 1
+
+    const seconds = Math.floor((time / 1000) % 60);
+    const minutes = Math.floor((time / 1000 / 60) % 60);
+    const hours = Math.floor((time / 1000 / 60 / 60) % 100);
+    return { seconds, minutes, hours };
+  },
+};
+
+refs.startBtn.addEventListener('click', event => {
+  stopwatch.start();
+});
+refs.stopBtn.addEventListener('click', event => {
+  stopwatch.stop();
+});
