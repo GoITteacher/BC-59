09446e2f9ab86d87e5ba8eb14cc696813c9f3bf8
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-09-08 02:06:00 +0300

    update

--- src/js/modules/heroesAPI.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  headers: {
-    'X-RapidAPI-Key': '9b3ff61931msh1b42d77d34e33dap1c29cajsn3d3169e0e2f4',
-    'X-RapidAPI-Host': 'superhero-search.p.rapidapi.com',
-  },
-};
--- src/js/modules/nbaAPI.js
+++
@@ -1,6 +0,0 @@
-import { refs } from './refs';
-import playerCardTpl from '../../templates/player-card.hbs';
-
-export function createMarkupForPlayer(player) {
-  refs.cardContainer1.innerHTML = playerCardTpl(player);
-}
